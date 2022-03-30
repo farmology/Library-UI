cd986bf149720ce8c56157db8ed8e0c9b78c688a
farmology <abkim03@gmail.com>
2022-03-30 00:59:40 -0700

    Add display for library
    Add a loop to write details from book object in library array into a
    list item and append clone of the book node to the bookshelf element.
    This displays the books in the library array on the document.

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-function Book (title, author, pages, read){
+let Library = [
+    {
+        title: 'Politics of Waystar', 
+        author: 'Shiv Roy', 
+        pages: 394, 
+        read: true,
+    }, 
+    {
+        title: 'Succession', 
+        author: 'Kendall Roy', 
+        pages: 281, 
+        read: true,
+    }, 
+    {
+        title: 'Hedonism', 
+        author: 'Romulus Roy', 
+        pages: 202, 
+        read: false,
+    },
+];
+const bookshelf = document.querySelector(".bookshelf");
+const book = document.createElement("li");
+
+book.classList.add('book');
+
+Library.map(object => {
+    book.innerHTML = 
+        `
+        <ul>
+            <li>
+            Title: ${object.title}
+            </li>
+            <li>
+            Author: ${object.author}
+            </li>
+            <li>
+            Pages: ${object.pages}
+            </li>
+            <li>
+            Read: ${object.read}
+            </li>
+         </ul>
+         `;
+         bookshelf.appendChild(book.cloneNode(true));
+        }
+    
+    )
+
+function Book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
@@ -6,4 +54,8 @@ function Book (title, author, pages, read){
     this.info = function() {
         console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${ this.read ? 'read' : 'not read yet'}`) 
     }
+}
+
+function addBook(title, author, pages, read) {
+    Library.push(Book(title, author, pages, read));
 }
