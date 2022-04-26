923bb2e2924424a7c705b2b60b4ff5422a9d20d5
farmology <abkim03@gmail.com>
2022-04-25 22:53:11 -0700

    Change display & addBook to work with localstorage
    Change the library display to work from localstorage and add lines to
    the addBook function to update the array in localstorage and insert html
    to update the library display.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let Library = [
+const Library = [
     {
         title: 'Politics of Waystar', 
         author: 'Shiv Roy', 
@@ -18,6 +18,7 @@ let Library = [
         read: false,
     },
 ];
+localStorage.setItem('Library', JSON.stringify(Library));
 const bookshelf = document.querySelector(".bookshelf");
 const book = document.createElement("li");
 const openFormBtn = document.getElementById('openForm');
@@ -41,7 +42,64 @@ window.addEventListener('click', closeModal);
 
 book.classList.add('book');
 
-Library.map(object => {
+
+// Library.map(object => {
+//     book.innerHTML = 
+//         `<ul>
+//             <li>
+//             Title: ${object.title}
+//             </li>
+//             <li>
+//             Author: ${object.author}
+//             </li>
+//             <li>
+//             Pages: ${object.pages}
+//             </li>
+//             <li>
+//             Read: ${object.read}
+//             </li>
+//          </ul>`;
+//     bookshelf.appendChild(book.cloneNode(true));
+//         }
+    
+//     )
+
+class Book{
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+    info() {
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${ this.read ? 'read' : 'not read yet'}`) 
+    }
+}
+
+function addBook(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    Library.push(newBook);
+    localStorage.setItem('Library', JSON.stringify(Library));
+    const book = document.createElement("li");
+    book.innerHTML = 
+        `<ul>
+            <li>
+            Title: ${newBook.title}
+            </li>
+            <li>
+            Author: ${newBook.author}
+            </li>
+            <li>
+            Pages: ${newBook.pages}
+            </li>
+            <li>
+            Read: ${newBook.read}
+            </li>
+         </ul>`;
+    bookshelf.appendChild(book.cloneNode(true));
+}
+
+JSON.parse(localStorage.getItem('Library')).map(object => {
     book.innerHTML = 
         `<ul>
             <li>
@@ -58,20 +116,5 @@ Library.map(object => {
             </li>
          </ul>`;
     bookshelf.appendChild(book.cloneNode(true));
-        }
-    
+}
     )
-
-function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${ this.read ? 'read' : 'not read yet'}`) 
-    }
-}
-
-function addBook(title, author, pages, read) {
-    Library.push(Book(title, author, pages, read));
-}
