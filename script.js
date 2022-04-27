5a3e99868d1b7d6dc09ae1633954866be1e76813
farmology <abkim03@gmail.com>
2022-04-26 22:50:04 -0700

    Update submit button to update library array
    Add event listener to form to listen for a submit action. Upon submit,
    library array updates and displays new book. Prevent default added to
    actually fire the addBook function.

--- script.js
+++ script.js
@@ -18,11 +18,78 @@ const Library = [
         read: false,
     },
 ];
-localStorage.setItem('Library', JSON.stringify(Library));
+
+
+
+// JSON.parse(localStorage.getItem('Library')).map(object => {
+//     book.innerHTML = 
+//         `<ul>
+//             <li>
+//             Title: ${object.title}
+//             </li>
+//             <li>
+//             Author: ${object.author}
+//             </li>
+//             <li>
+//             Pages: ${object.pages}
+//             </li>
+//             <li>
+//             Read: ${object.read}
+//             </li>
+//          </ul>`;
+//     bookshelf.appendChild(book.cloneNode(true));
+// });
+
 const bookshelf = document.querySelector(".bookshelf");
 const book = document.createElement("li");
 const openFormBtn = document.getElementById('openForm');
 const modal = document.getElementById('simpleModal');
+const submitBtn = document.getElementById('submitbtn');
+const form = document.getElementById('popupform');
+
+if (!localStorage.getItem('Library')) {
+    JSON.parse(localStorage.getItem('Library')).map(object => {
+        const book = document.createElement("li");
+        book.innerHTML = 
+            `<ul>
+                <li>
+                Title: ${object.title}
+                </li>
+                <li>
+                Author: ${object.author}
+                </li>
+                <li>
+                Pages: ${object.pages}
+                </li>
+                <li>
+                Read: ${object.read}
+                </li>
+             </ul>`;
+        bookshelf.appendChild(book.cloneNode(true));
+    });
+    
+} else {
+    localStorage.setItem('Library', JSON.stringify(Library));
+    JSON.parse(localStorage.getItem('Library')).map(object => {
+        const book = document.createElement("li");
+        book.innerHTML = 
+            `<ul>
+                <li>
+                Title: ${object.title}
+                </li>
+                <li>
+                Author: ${object.author}
+                </li>
+                <li>
+                Pages: ${object.pages}
+                </li>
+                <li>
+                Read: ${object.read}
+                </li>
+             </ul>`;
+        bookshelf.appendChild(book.cloneNode(true));
+    });
+};
 
 function openModal(){
     modal.style.display = 'block';
@@ -36,11 +103,22 @@ function closeModal(e){
 
 openFormBtn.addEventListener('click', openModal);
 window.addEventListener('click', closeModal);
+form.addEventListener('submit', function(event) {
+    console.log('testing');
+    console.log(document.getElementsByTagName('input').value);
+    let title = document.getElementsByTagName('input')[0].value;
+    let author = document.getElementsByTagName('input')[1].value;
+    let pages = document.getElementsByTagName('input')[2].value;
+    let read = document.getElementsByTagName('input')[3].value;
+    addBook(title, author, pages, read);
+    event.preventDefault();
+});
 
 
 
 
-book.classList.add('book');
+
+// book.classList.add('book');
 
 
 // Library.map(object => {
@@ -99,22 +177,3 @@ function addBook(title, author, pages, read) {
     bookshelf.appendChild(book.cloneNode(true));
 }
 
-JSON.parse(localStorage.getItem('Library')).map(object => {
-    book.innerHTML = 
-        `<ul>
-            <li>
-            Title: ${object.title}
-            </li>
-            <li>
-            Author: ${object.author}
-            </li>
-            <li>
-            Pages: ${object.pages}
-            </li>
-            <li>
-            Read: ${object.read}
-            </li>
-         </ul>`;
-    bookshelf.appendChild(book.cloneNode(true));
-}
-    )
